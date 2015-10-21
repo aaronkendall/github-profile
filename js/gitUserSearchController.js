0bb46f4bef4973822727dc4dc6dc03fe618b1120
Aaron <aarongkendall@gmail.com>
2015-10-21 11:31:17 +0100

    Github search API

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,4 +1,14 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
   var searchResult;
   var searchTerm;
+  var self = this;
+  var access_token = gitAccessToken;
+  var searchResource = $resource('https://api.github.com/search/users');
+
+  self.doSearch = function() {
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm, access_token: gitAccessToken }
+    );
+  };
+
 }]);
